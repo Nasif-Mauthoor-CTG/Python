e9976e688e75fd3ec1b088aec3ce123e330418e2
Nasif Mauthoor <nasifmrmonline@gmail.com>
2022-06-29 04:04:58 -0400

    Project 4 program skeleton

---
+++ main.py
@@ -0,0 +1,32 @@
+# Project 4
+# Coin Toss Game
+# Goal: Create a simple game that will flip a coin randomly.
+#       If user picks the same side as the program, they win.
+
+
+# This is your main function
+# Main is the start of your program
+# Use this to run the sequence of functions we
+# need to complete this project.
+def main():
+
+
+# Print to user to pick
+# Returns 0 for heads or 1 for tails
+def prompt_user():
+
+
+# Randomly pick heads or tails
+# Returns 0 for heads or 1 for tails
+def flip_coin():
+
+
+# This function returns a boolean
+# Returns true for a win and false for a loss
+def evaluate_game(user_input, random_input):
+
+
+# Run this file if it's the main file
+# We will find out the reason for this later
+if __name__ == '__main__':
+    main()
