d7b785014fb04fe3e89e78f5bf4084fb04c5cf28
Nasif Mauthoor <nasifmrmonline@gmail.com>
2022-07-04 18:36:12 -0400

    Project 5 Template

---
+++ main.py
@@ -0,0 +1,33 @@
+from random import randint # We can call randint without writing random.randint()
+
+# Rock paper scissors game
+# User and computer pick rock paper scissors.
+# Rock beats scissors, paper beats rock and scissors beat paper.
+
+# Guideline:
+# You are only allowed to print in main.
+# Follow the instructions for every function.
+# Put everything together in main.
+
+# Start
+# Print messages to user and run the other functions in order
+def main():
+    print('Welcome to the snake game.')
+
+# Get input from user (Hint: input())
+# Return 'rock', 'paper' or 'scissors'
+def pick_stance():
+
+
+# Get a randomly generated answer (Hint: randint(
+# Return 'rock', 'paper' or 'scissors'
+def generate_stance():
+
+
+# Return 'user', 'computer' or 'tie'
+def evaluate_winner():
+
+
+# Run this file if it's the main file
+if __name__ == '__main__':
+    main()
