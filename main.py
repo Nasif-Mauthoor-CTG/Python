6e325de2851c840e2b64cabbd289be3897edefad
Nasif Mauthoor <nasifmrmonline@gmail.com>
2022-07-06 14:07:20 -0400

    Project 5 Complete

--- main.py
+++ main.py
@@ -13,19 +13,53 @@ from random import randint # We can call randint without writing random.randint(
 # Print messages to user and run the other functions in order
 def main():
     print('Welcome to the snake game.')
+    winner = 'tie'
+
+    while winner == 'tie':
+        print('Pick rock/paper/scissors')
+        user_input = pick_stance()
+
+        comp_input = generate_stance()
+        print('The CPU picked ', comp_input)
+
+        winner = evaluate_winner(user_input, comp_input)
+        print('You ', winner)
+
 
 # Get input from user (Hint: input())
 # Return 'rock', 'paper' or 'scissors'
 def pick_stance():
+    choice = input()
+    return choice
 
 
 # Get a randomly generated answer (Hint: randint(
 # Return 'rock', 'paper' or 'scissors'
 def generate_stance():
+    array = ['rock', 'paper', 'scissors']
+    number = randint(0, 2)
+    return array[number]
 
 
 # Return 'user', 'computer' or 'tie'
-def evaluate_winner():
+def evaluate_winner(user, cpu):
+    if user == cpu:
+        return 'tie'
+    if user == 'rock':
+        if cpu == 'paper':
+            return 'lose'
+        else :
+            return 'win'
+    if user == 'scissors':
+        if cpu == 'rock':
+            return 'lose'
+        else :
+            return 'win'
+    if user == 'paper':
+        if cpu == 'scissors':
+            return 'lose'
+        else :
+            return 'win'
 
 
 # Run this file if it's the main file
